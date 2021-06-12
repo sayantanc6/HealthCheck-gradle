591bd5d6ac467393e6c69aa55f36100c92797f2f
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2021-06-12 18:44:07 +0530

    Add files via upload
    na

---
+++ HealthIndicator-1/src/main/java/dummy/HealthIndicator1Application.java
@@ -0,0 +1,13 @@
+package dummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthIndicator1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthIndicator1Application.class, args);
+	}
+
+}
